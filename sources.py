f2e4b6aa05780d1ef856f8ebf99212a0e5cbd6a8
Dave Young <dave.andrew.young@gmail.com>
2023-08-18 12:57:15 -0500

    try using json instead

--- sources.py
+++ sources.py
@@ -13,61 +13,69 @@ def get_sources(d: str):
     return [Source(x) for x in glob.glob(f"{os.path.join(JOBS_ROOT,d,'library/postings/source')}/**/*.mov",recursive=True)]
 
 def start_transcode(src: Source):
-    k8s = Kubernetes()
-    job_container = k8s.create_container(
-        image="registry.carbonvfx.com/engineering/postings:transcode-latest",
-        name="transcode",
-        pull_policy="Always",
-        args=src
-    )
-    job_pod_spec = k8s.create_pod_template(
-        pod_name=f"{src.filename.split('.')[0]}-{src.date}-{src.time}",
-        container=job_container
-    )
-    job_spec = k8s.create_job(
-        job_name=f"{src.filename.split('.')[0]}-{src.date}-{src.time}",
-        pod_template=job_pod_spec
-    )
     logging.info(f"creating job: {src.filename.split('.')[0]}-{src.date}-{src.time}")
+    job_spec = {
+        "apiVersion": "batch/v1",
+        "kind": "Job",
+        "metadata":{
+            "name": f"{src.filename.split('.')[0]}-{src.date}-{src.time}",
+            "namespace": "postings"
+        },
+        "spec":{
+            "template":{
+                "spec":{
+                    "restartPolicy": "Never",
+                    "volumes":[
+                        {
+                            "name": "secrets-store-inline",
+                            "csi":{
+                                "driver": "secrets-store.csi.k8s.io",
+                                "readOnly": "true",
+                                "volumeAttributes":{
+                                    "secretProviderClass": "regcred",
+                                },
+                            }
+                        },
+                        {
+                            "name": "weka-jobs",
+                            "nfs":{
+                                "server": "10.70.50.117",
+                                "path": "/vfx/vfx/Jobs/tmobile_metro_235890",
+                                "readOnly": "false",
+                            }
+                        }
+                    ],
+                    "imagePullSecrets":[
+                        {
+                            "name": "regcred"
+                        },
+                    ],
+                    "serviceAccountName": "image-puller",
+                    "containers":[
+                        {
+                            "name": f"{src.filename.split('.')[0]}-{src.date}-{src.time}",
+                            "image": "registry.carbonvfx.com/engineering/postings:transcode-latest",
+                            "volumeMounts":[
+                                {   
+                                    "name": "secrets-store-inline",
+                                    "mountPath": "/secrets-store",
+                                    "readOnly": "true"
+                                },
+                                {   
+                                    "name": "weka-jobs",
+                                    "mountPath": "/mnt/jobs/tmobile_metro_235890",
+                                    "readOnly": "false"
+                                }
+                            ]
+                        }
+                    ]
+                }
+            }
+        }
+    }
     batch_api = client.BatchV1Api()
     batch_api.create_namespaced_job("postings", job_spec)
     return
-
-class Kubernetes:
-    def __init__(self):
-        # Init Kubernetes
-        self.core_api = client.CoreV1Api()
-        self.batch_api = client.BatchV1Api()
-
-    @staticmethod
-    def create_container(image, name, pull_policy, args):
-        container = client.V1Container(
-            image=image,
-            name=name,
-            image_pull_policy=pull_policy,
-            args=[args]
-        )
-        return container
-
-    @staticmethod
-    def create_pod_template(pod_name, container):
-        pod_template = client.V1PodTemplateSpec(
-            spec=client.V1PodSpec(restart_policy="Never", containers=[container]),
-            metadata=client.V1ObjectMeta(name=pod_name, labels={"pod_name": pod_name}),
-        )
-        return pod_template
-
-    @staticmethod
-    def create_job(job_name, pod_template):
-        metadata = client.V1ObjectMeta(name=job_name, labels={"job_name": job_name})
-        job = client.V1Job(
-            api_version="batch/v1",
-            kind="Job",
-            metadata=metadata,
-            spec=client.V1JobSpec(backoff_limit=0, template=pod_template),
-        )
-        return job
-
 
 if __name__ == "__main__":
     while True:
